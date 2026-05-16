7069854ea99c78e33f2e5050e92c1455c1bbd5a7
versus0 <AkemyHomura@hotmail.com>
2026-05-16 21:47:33 +0800

    1

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -5,7 +5,7 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "公告",
 
 	// 公告内容
-	content: "欢迎来到我的博客！这是一则示例公告。",
+	content: "欢迎来到我的博客！如果您喜欢我的内容，欢迎赞助我！您的支持就是我最大的动力！！！",
 
 	// 是否允许用户关闭公告
 	closable: true,
