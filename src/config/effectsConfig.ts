ba38aba9834dfb7a20958780765263a7b06b0239
versus0 <AkemyHomura@hotmail.com>
2026-06-10 23:45:58 +0800

    搜索

--- src/config/effectsConfig.ts
+++ src/config/effectsConfig.ts
@@ -36,19 +36,19 @@ export const sakuraConfig: SakuraConfig = {
 		// 水平移动
 		horizontal: {
 			// 水平移动速度最小值
-			min: -1.7,
+			min: -1.0,
 			// 水平移动速度最大值
-			max: -1.2,
+			max: -0.3,
 		},
 		// 垂直移动
 		vertical: {
 			// 垂直移动速度最小值
-			min: 1.5,
+			min: 0.3,
 			// 垂直移动速度最大值
-			max: 2.2,
+			max: 1.0,
 		},
 		// 旋转速度
-		rotation: 0.03,
+		rotation: 0.01,
 		// 消失速度，不应大于最小不透明度
 		fadeSpeed: 0.03,
 	},
